789aca584f81083830705f9a745902f9dfc9e2bd
caley8535 <50510161+caley8535@users.noreply.github.com>
2023-04-21 03:32:39 +0900

    breeding 수정

--- breeding.h
+++ breeding.h
@@ -1,22 +1,56 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <algorithm>
+#include <omp.h>
 using namespace std;
 
-// 적합도 상위 10개 개체 중 두 개 선택
-void breeding(char A[], char B[])
+const int GENE_SIZE = 50;
+const int ARRAY_SIZE = 201;
+
+// 적합도 상위 10개 개체 선정 -> top_arrays에 순서대로 넣음
+vector<int*> selectTopArrays(int arr[GENE_SIZE][ARRAY_SIZE])
 {
-	const int ARRAY_SIZE = 200;
+	sort(arr, arr + 50, [](const int* a, const int* b) {return a[0] > b[0]; });
+
+	vector<int*> top_arrays;
+	for (int i = 0; i < 10; i++)
+	{
+		top_arrays.push_back(arr[i]);
+	}
+
+	return top_arrays;
+}
+
+void breeding(vector<int*>& top_arrays)
+{
 	const int NUM_EXCHANGES = 100;
+	int half_A[NUM_EXCHANGES], half_B[NUM_EXCHANGES];
+	int a[ARRAY_SIZE], b[ARRAY_SIZE];
+	int c[GENE_SIZE][ARRAY_SIZE];
 
 	vector<int> detectIndex_a, detectIndex_b;
 	random_device rd;
 	mt19937 gen(rd());
-	uniform_int_distribution<int> rand(0, ARRAY_SIZE - 1);
+	uniform_int_distribution<int> rand(1, ARRAY_SIZE - 1);
+	uniform_int_distribution<int> toprand(0, top_arrays.size());
 
-	char half_A[NUM_EXCHANGES], half_B[NUM_EXCHANGES];
-	char a[ARRAY_SIZE], b[ARRAY_SIZE];
+	// top_arrays에서 무작위로 두 개의 배열 선택
+	int idx1 = toprand(gen);
+	int idx2 = toprand(gen);
+	while (idx2 == idx1)
+	{
+		idx2 = toprand(gen);
+	}
 
+	// 선택된 배열을 각각 A, B에 저장
+	int A[ARRAY_SIZE], B[ARRAY_SIZE];
+	for (int i = 0; i < ARRAY_SIZE; i++)
+	{
+		A[i] = top_arrays[idx1][i];
+		B[i] = top_arrays[idx2][i];
+	}
+	
 	for (int i = 0; i < ARRAY_SIZE; i++)
 	{
 		a[i] = A[i];
@@ -24,28 +58,50 @@ void breeding(char A[], char B[])
 	}
 
 	int randIndex;
-
-	for (int i = 0; i < NUM_EXCHANGES; i++)
+	int childcount = 0;
+	#pragma omp_parallel num_threads(4)
 	{
-		do {
-			randIndex = rand(gen);
-		} while (find(detectIndex_a.begin(), detectIndex_a.end(), randIndex) != detectIndex_a.end());
+		#pragma omp for
+		for (int count = 0; count < 25; count++)
+		{
+			for (int i = 0; i < NUM_EXCHANGES; i++)
+			{
+				do {
+					randIndex = rand(gen);
+				} while (find(detectIndex_a.begin(), detectIndex_a.end(), randIndex) != detectIndex_a.end());
 
-		detectIndex_a.push_back(randIndex);
+				detectIndex_a.push_back(randIndex);
 
-		half_A[i] = A[randIndex];
-		b[randIndex] = half_A[i];
+				half_A[i] = A[randIndex];
+				b[randIndex] = half_A[i];
+				c[childcount][i] = b[i];
+			}
+			childcount++;
+			detectIndex_a.clear();
+
+			for (int i = 0; i < NUM_EXCHANGES; i++)
+			{
+				do {
+					randIndex = rand(gen);
+				} while (find(detectIndex_b.begin(), detectIndex_b.end(), randIndex) != detectIndex_b.end());
+
+				detectIndex_b.push_back(randIndex);
+
+				half_B[i] = B[randIndex];
+				a[randIndex] = half_B[i];
+				c[childcount][i] = a[i];
+			}
+			childcount++;
+			detectIndex_b.clear();
+
+			if (childcount == 49)
+				break;
+		}
 	}
-
-	for (int i = 0; i < NUM_EXCHANGES; i++)
-	{
-		do {
-			randIndex = rand(gen);
-		} while (find(detectIndex_b.begin(), detectIndex_b.end(), randIndex) != detectIndex_b.end());
-
-		detectIndex_b.push_back(randIndex);
-
-		half_B[i] = B[randIndex];
-		a[randIndex] = half_B[i];
-	}
+	
 }
+
+/* main에서
+vector<int*> top_arrays = selectTopArrays(배열);
+breeding(top_arrays);
+*/
